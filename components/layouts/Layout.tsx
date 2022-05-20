f58e42f675a65898c0afbf56e4f8ba614c7a46b5
MiaFate <miapurplefroot@gmail.com>
2022-05-19 21:24:54 -0300

    49

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react"
+import Head from "next/head"
+import { Navbar } from '../ui';
+
+interface LayoutProps {
+    title?: string;
+    children: JSX.Element;
+}
+
+export const Layout: FC<LayoutProps> = ({ children, title }) => {
+    return (
+        <>
+            <Head>
+                <title>{title || 'Pokemon App'}</title>
+                <meta name="author" content="Mia Ramos" />
+                <meta name="description" content={`Informacion sobre el Pokemon ${title}`} />
+                <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+            </Head>
+
+            <Navbar />
+
+            <main style={{
+                padding: '0px 20px',
+            }}>
+                {children}
+            </main>
+        </>
+    )
+}
