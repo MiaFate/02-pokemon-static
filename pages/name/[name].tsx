995fb5985610569bb6eb4b350cc775cbfafd61ab
MiaFate <miapurplefroot@gmail.com>
2022-05-21 21:09:12 -0300

    incremental static regeneration

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -108,14 +108,16 @@ const PokemonByNamePage: NextPage<Props> = ({ pokemon }) => {
 
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
+
     const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
     const pokemonNames: string[] = data.results.map(pokemon => pokemon.name);
-
     return {
-        paths: pokemonNames.map(name => ({
-            params: { name }
-        })),
-        fallback: false
+        paths: pokemonNames.map(name => {
+            return {
+                params: { name }
+            }
+        }),
+        fallback: 'blocking',
     }
 }
 
@@ -130,10 +132,23 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
     const { name } = params as { name: string };
 
+    const pokemon = await getPokemonInfo(name);
+
+    if (!pokemon) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+
+    }
+
     return {
         props: {
-            pokemon: await getPokemonInfo(name)
-        }
+            pokemon
+        },
+        revalidate: 86400,
     }
 }
 
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -120,7 +120,8 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
         //         params: { id: '2' }
         //     },
         // ],
-        fallback: false
+        //fallback: false
+        fallback: 'blocking'
     }
 }
 
@@ -128,10 +129,22 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
     const { id } = params as { id: string };
 
+    const pokemon = await getPokemonInfo(id);
+
+    if (!pokemon) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
     return {
         props: {
-            pokemon: await getPokemonInfo(id)
-        }
+            pokemon
+        },
+        revalidate: 86400, //in seconds
     }
 };
 
